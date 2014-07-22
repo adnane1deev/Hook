26d49f8e490c8e9d0b04b4c61045b60d1c8a10d0
adnane1deev <adnane.deev@gmail.com>
2014-07-22 04:09:18 +0000

    adding more functionalities to download manager

--- bin/cli_download_manager.py
+++ bin/cli_download_manager.py
@@ -1,41 +1,90 @@
-__author__ = 'Asus'
-
+__author__ = 'Adnane Deev'
 
 import urllib2
+import types
+import pprint as pn
 
-types = {'application/zip': 'zip'}
+
+class idm_exception(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class download_manager(object):
+
+    def __init__(self):
+        self.__types = {'application/zip': 'zip', 'application/rar': 'rar'}
+        self.__URLS = []
+        self.__URL = ''
+
+    def setURL(self, _url):
+        self.__URL = _url
+
+    def setURLS(self, _urls):
+        self.__URLS.extend(_urls)
+
+    def addURL(self, _url):
+        self.__URLS.append(_url)
+
+    def __download(self, _url):
+        file_size = None
+        file_name = ""
+        http_connection = None
+        file_handler = None
+
+        while file_size is None:
+            http_connection = urllib2.urlopen(_url)
+            metadata = http_connection.info()
+            print pn.pprint(metadata.dict, indent="2")
+            file_name = _url.split('/')[-4]+"-"+_url.split('/')[-3]+"."+self.__types[metadata.getheaders("content-type")[0]]
+            file_handler = open(file_name, 'wb')
+            #print metadata.getheaders("Content-Type")
+            #print metadata.dict
+            try:
+                file_size = int(metadata.getheaders("Content-Length")[0])
+            except Exception:
+                print "Couldn't resolve the URL: trying again ..."
+
+        print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+        file_size_dl = 0
+        block_sz = 8192
+
+        while True:
+            __buffer = http_connection.read(block_sz)
+            if not __buffer:
+                break
+
+            file_size_dl += len(__buffer)
+            file_handler.write(__buffer)
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status += chr(8)*(len(status)+1)
+            print status,
+
+        file_handler.close()
+        print
+
+    def startQueue(self, _url_s):
+        try:
+            if isinstance(_url_s, types.ListType):
+                print 'list'
+                for url in _url_s:
+                    self.__download(url)
+
+            elif isinstance(_url_s, types.StringType):
+                print 'str'
+                self.__download(_url_s)
+
+            else:
+                raise idm_exception('sss')
+        except idm_exception as e:
+            print e.value
+
 urls = ["https://github.com/bower/registry/archive/master.zip",
         "https://github.com/bower/bower/archive/master.zip",
         "https://github.com/zendframework/ZendSkeletonApplication/archive/master.zip"]
-#url = "http://nodejs.org/dist/v0.10.29/x64/node-v0.10.29-x64.msi"
-print
-for url in urls:
-    u = urllib2.urlopen(url)
-    meta = u.info()
-
-    file_name = url.split('/')[-4]+"-"+url.split('/')[-3]+"."+types[meta.getheaders("Content-Type")[0]]
-
-    f = open(file_name, 'wb')
-
-    #print meta.getheaders("Content-Type")
-    #print meta.dict
-
-    file_size = int(meta.getheaders("Content-Length")[0])
-
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        __buffer = u.read(block_sz)
-        if not __buffer:
-            break
-
-        file_size_dl += len(__buffer)
-        f.write(__buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status += chr(8)*(len(status)+1)
-        print status,
-
-    f.close()
-    print "\n"
+idm = download_manager()
+idm.startQueue("https://github.com/zendframework/ZendSkeletonApplication/archive/master.zip")
