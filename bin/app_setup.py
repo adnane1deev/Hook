af8d25d74dbbfcb25bcdfb454125d834644bc1bc
adnane1deev <adnane.deev@gmail.com>
2014-08-08 05:05:59 +0000

    Append G(app) to os $PATH

--- bin/app_setup.py
+++ bin/app_setup.py
@@ -12,6 +12,13 @@ def setup():
 
     if not os.path.exists(app_tree):
         op.create_tree(app_tree)
-
+        file_absolute_path = os.path.abspath(__file__)
+        base_dir = os.path.split(file_absolute_path)[0]
+        hook_absolute_path = base_dir.rsplit(op.separator(), 1)[0]
+        append_hook_to_sys_path(hook_absolute_path)
     else:
         print hook.application_name + " is already setup"
+
+
+def append_hook_to_sys_path(_path):
+    os.environ['PATH'] += os.pathsep + _path
